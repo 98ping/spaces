e1702e123185f0e6af79c863f3e0981196c0789e
Max <67809373+98ping@users.noreply.github.com>
2024-05-26 17:27:29 -0700

    feat: add the ability to zip an entire directory at once

--- src/main/java/ltd/matrixstudios/spaces/environments/routes/EnvironmentController.java
+++ src/main/java/ltd/matrixstudios/spaces/environments/routes/EnvironmentController.java
@@ -4,6 +4,11 @@ import com.google.gson.JsonObject;
 import ltd.matrixstudios.spaces.SpacesApplication;
 import ltd.matrixstudios.spaces.environments.Environment;
 import ltd.matrixstudios.spaces.environments.files.WrappedFile;
+import ltd.matrixstudios.spaces.util.ZipUtility;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -50,6 +60,79 @@ public class EnvironmentController {
         modelAndView.addObject("environment", environment);
 
         return modelAndView;
+    }
+
+    /**
+     * This function is a blatant attempt at covering my tracks when it comes to
+     * backend error handling lol
+     *
+     * Don't worry though, JavaScript will handle it!
+     */
+    @RequestMapping(value = {"/environment/zip/{id}"}, method = {RequestMethod.GET})
+    public ResponseEntity<Resource> zipEnvironment(@PathVariable String id) {
+        UUID formattedId = UUID.fromString(id);
+        Environment environment = SpacesApplication.instance.getEnvironmentManager().getEnvironmentById(formattedId);
+
+        // Basic handling of the environment path stuff
+        if (environment == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        if (environment.getPathToEnvironment() == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        File environmentDestination = new File(environment.getPathToEnvironment());
+        File[] files = environmentDestination.listFiles();
+
+        if (files == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        // Initiate the directories for both the zip action and the file that the zip
+        // will go to
+        String zipDir = SpacesApplication.instance.getDirectoryManager().getEnvironmentPath().getPath()
+                + "\\" + environment.getStringedIdentifier() + "\\zip";
+        String outputPath = zipDir + "\\" + environment.getStringedIdentifier() + ".zip";
+
+        String[] filesToNames = Arrays.stream(files).map(File::getPath).toArray(String[]::new);
+        File outputPathFile = new File(outputPath);
+        File outputZipDir = new File(zipDir);
+
+        if (!outputZipDir.exists()) {
+            outputZipDir.mkdirs();
+        }
+
+        if (!outputPathFile.exists()) {
+            try {
+                outputPathFile.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ResponseEntity.status(404).body(null);
+            }
+        }
+
+        // Zip it!
+        try {
+            ZipUtility.zip(filesToNames, outputPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(404).body(null);
+        }
+
+
+        // Send the input stream back to the webpage for download
+        try {
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(outputPathFile));
+
+            return ResponseEntity.ok()
+                    .contentLength(outputPathFile.length())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } catch (FileNotFoundException e) {
+            System.out.println("final path didnt exist");
+            return ResponseEntity.status(404).body(null);
+        }
     }
 
 }

676e81fc4245bd00a24da7a5c2b45cb195fe2b28
Max <maxrayne98@gmail.com>
2024-08-11 00:58:35 -0700

    feat: finally fix the password box on login page and also add some new routes

---
+++ src/main/java/ltd/matrixstudios/spaces/environments/routes/user/POSTEnvironmentDelegateUserPermission.java
@@ -0,0 +1,56 @@
+package ltd.matrixstudios.spaces.environments.routes.user;
+
+import com.google.gson.JsonObject;
+import jakarta.servlet.http.HttpServletRequest;
+import ltd.matrixstudios.spaces.SpacesApplication;
+import ltd.matrixstudios.spaces.environments.Environment;
+import ltd.matrixstudios.spaces.user.UserRepository;
+import ltd.matrixstudios.spaces.user.model.SpaceUser;
+import ltd.matrixstudios.spaces.user.model.SpaceUserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+public class POSTEnvironmentDelegateUserPermission {
+
+    @RequestMapping(value = {"/environment/{id}/roles/delegate"}, method = {RequestMethod.POST})
+    public String delegateRoleToEnvironment(HttpServletRequest request, @PathVariable String id, @RequestBody String payload) {
+        JsonObject jsonObject = SpacesApplication.GSON.fromJson(payload, JsonObject.class);
+
+        // Make sure requester has the permissions they need
+        SpaceUser requester = (SpaceUser) request.getAttribute("user");
+
+        if (requester == null || !requester.has(SpaceUserRole.ADMIN)) {
+            throw new RuntimeException("You do not have permission to request this endpoint!");
+        }
+
+        Environment environment = SpacesApplication.instance.getEnvironmentManager().getEnvironmentById(UUID.fromString(id));
+
+        // A bit better safety for enums. Messy but it works
+        SpaceUserRole role = Arrays.stream(SpaceUserRole.values())
+                .filter(value -> value.name().equals(jsonObject.get("role").getAsString()))
+                .findFirst().orElse(null);
+
+        if (role == null) {
+            throw new RuntimeException("Unable top find a role by this name!");
+        }
+
+        UUID userId = UUID.fromString(jsonObject.get("userId").getAsString());
+        List<SpaceUserRole> userRoles = environment.getUserRoles().getOrDefault(userId, new ArrayList<>());
+        userRoles.add(role);
+
+        environment.getUserRoles().put(userId, userRoles);
+        environment.save();
+
+        return "redirect:/home";
+    }
+}
